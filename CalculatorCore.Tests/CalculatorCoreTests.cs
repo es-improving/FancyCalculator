308179562db0e6f29a23f7d03d15d493c8f874a0
Eric Sowell <eric.sowell@improving.com>
2021-07-12 16:43:39 -0500

    subtraction, multiplication, and division support added

--- CalculatorCore.Tests/CalculatorCoreTests.cs
+++ CalculatorCore.Tests/CalculatorCoreTests.cs
@@ -34,7 +34,28 @@ namespace CalculatorCore.Tests
             Assert.AreEqual("The second number, 'hello', was not a valid number.", result.ErrorMessage);
         }
 
+        [TestMethod]
+        public void SubtractANumberFromAnother()
+        {
+            var calc = new Calculator();
+            var result = calc.Evaluate("7 - 687");
+            Assert.AreEqual(-680m, result.Result);
+        }
 
+        [TestMethod]
+        public void DivisionWorks()
+        {
+            var calc = new Calculator();
+            var result = calc.Evaluate("8 / 4");
+            Assert.AreEqual(2m, result.Result);
+        }
 
+        [TestMethod]
+        public void MultiplicationWorks()
+        {
+            var calc = new Calculator();
+            var result = calc.Evaluate("2 * 12");
+            Assert.AreEqual(24m, result.Result);
+        }
     }
 }
--- CalculatorCore/Calculator.cs
+++ CalculatorCore/Calculator.cs
@@ -24,7 +24,29 @@ namespace CalculatorCore
                 return new EvaluationResult { ErrorMessage = $"The second number, '{expressionBits[2]}', was not a valid number." };
             }
 
-            return new EvaluationResult { Result = firstOperand + secondOperand };
+            string op = expressionBits[1];
+            decimal result;
+
+            switch (op)
+            {
+                case "+":
+                    result = firstOperand + secondOperand;
+                    break;
+                case "-":
+                    result = firstOperand - secondOperand;
+                    break;
+                case "/":
+                    result = firstOperand / secondOperand;
+                    break;
+                case "*":
+                    result = firstOperand * secondOperand;
+                    break;
+                default:
+                    // TODO: My code is bad because this is kinda needed, so I should fix it at some point
+                    throw new NotImplementedException($"The following operator was used, but I don't understand it: {op}");
+            }
+
+            return new EvaluationResult { Result = result };
         }
     }
 }
