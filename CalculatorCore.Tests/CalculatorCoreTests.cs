c2ecbb19abe73a3b252e155831a62a9d7507e94d
Eric Sowell <eric.sowell@improving.com>
2021-07-12 17:00:01 -0500

    added test initialize for calc tests

--- CalculatorCore.Tests/CalculatorCoreTests.cs
+++ CalculatorCore.Tests/CalculatorCoreTests.cs
@@ -5,14 +5,19 @@ namespace CalculatorCore.Tests
     [TestClass]
     public class CalculatorCoreTests
     {
+        private Calculator _calc;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _calc = new Calculator();
+        }
+
         [TestMethod]
         public void AddTwoNumbers()
         {
-            // arrange
-            var calc = new Calculator();
-
             // act
-            EvaluationResult result = calc.Evaluate("6 + 8");
+            EvaluationResult result = _calc.Evaluate("6 + 8");
             
             // assert
             Assert.AreEqual(14m, result.Result);
@@ -21,40 +26,36 @@ namespace CalculatorCore.Tests
         [TestMethod]
         public void FirstOperandMustBeNumber()
         {
-            var calc = new Calculator();
-            var result = calc.Evaluate("fdsalkj + 8");
+            var result = _calc.Evaluate("fdsalkj + 8");
+            var wut = 2;
             Assert.AreEqual("The first number, 'fdsalkj', was not a valid number.", result.ErrorMessage);
         }
 
         [TestMethod]
         public void SecondOperandMustBeNumber()
         {
-            var calc = new Calculator();
-            var result = calc.Evaluate("7 + hello");
+            var result = _calc.Evaluate("7 + hello");
             Assert.AreEqual("The second number, 'hello', was not a valid number.", result.ErrorMessage);
         }
 
         [TestMethod]
         public void SubtractANumberFromAnother()
         {
-            var calc = new Calculator();
-            var result = calc.Evaluate("7 - 687");
+            var result = _calc.Evaluate("7 - 687");
             Assert.AreEqual(-680m, result.Result);
         }
 
         [TestMethod]
         public void DivisionWorks()
         {
-            var calc = new Calculator();
-            var result = calc.Evaluate("8 / 4");
+            var result = _calc.Evaluate("8 / 4");
             Assert.AreEqual(2m, result.Result);
         }
 
         [TestMethod]
         public void MultiplicationWorks()
         {
-            var calc = new Calculator();
-            var result = calc.Evaluate("2 * 12");
+            var result = _calc.Evaluate("2 * 12");
             Assert.AreEqual(24m, result.Result);
         }
     }
