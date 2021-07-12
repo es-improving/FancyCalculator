422a652642b279561f4db256ce493bf97a28be56
Eric Sowell <eric.sowell@improving.com>
2021-07-12 16:17:57 -0500

    added tests for both initial failure paths

--- CalculatorCore.Tests/CalculatorCoreTests.cs
+++ CalculatorCore.Tests/CalculatorCoreTests.cs
@@ -17,5 +17,24 @@ namespace CalculatorCore.Tests
             // assert
             Assert.AreEqual(14m, result.Result);
         }
+
+        [TestMethod]
+        public void FirstOperandMustBeNumber()
+        {
+            var calc = new Calculator();
+            var result = calc.Evaluate("fdsalkj + 8");
+            Assert.AreEqual("The first number, 'fdsalkj', was not a valid number.", result.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void SecondOperandMustBeNumber()
+        {
+            var calc = new Calculator();
+            var result = calc.Evaluate("7 + hello");
+            Assert.AreEqual("The second number, 'hello', was not a valid number.", result.ErrorMessage);
+        }
+
+
+
     }
 }
