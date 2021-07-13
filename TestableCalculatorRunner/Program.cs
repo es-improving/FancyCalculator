06f9a145d2f6b5d44d7a2354f82908912e144a60
Eric Sowell <eric.sowell@improving.com>
2021-07-13 10:41:38 -0500

    console now doesn't stop after every problem

--- TestableCalculatorRunner/Program.cs
+++ TestableCalculatorRunner/Program.cs
@@ -9,19 +9,28 @@ namespace TestableCalculatorRunner
         {
             var calculator = new Calculator();
 
-            Console.WriteLine("Give me math or give me death!");
-            string input = Console.ReadLine();
-
-            var output = calculator.Evaluate(input);
-
-            if (String.IsNullOrWhiteSpace(output.ErrorMessage))
+            while(true)
             {
-                Console.WriteLine(output.Result);
+                Console.WriteLine("Give me math or give me death!");
+                string input = Console.ReadLine();
+
+                if (input == "exit")
+                {
+                    break;
+                }
+
+                var output = calculator.Evaluate(input);
+
+                if (String.IsNullOrWhiteSpace(output.ErrorMessage))
+                {
+                    Console.WriteLine(output.Result);
+                }
+                else
+                {
+                    Console.WriteLine("\u001b[31m" + output.ErrorMessage + "\u001b[0m");
+                }
             }
-            else
-            {
-                Console.WriteLine("\u001b[31m" + output.ErrorMessage + "\u001b[0m");
-            }
+
         }
     }
 }
